dc00cca2a376670a1dcc44e04e4e1837cad9d844
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-12-02 13:53:41 +0100

    test(lib): add tests for cross stack references

--- packages/cdktf/test/app.test.ts
+++ packages/cdktf/test/app.test.ts
@@ -4,6 +4,7 @@ import {
   TerraformStack,
   TerraformResource,
   Testing,
+  DataTerraformRemoteStateLocal,
 } from "../lib";
 
 import { version } from "../package.json";
@@ -12,6 +13,9 @@ import path = require("path");
 import os = require("os");
 import { Aspects } from "../lib/aspect";
 import { IConstruct } from "constructs";
+import { setupJest } from "../lib/testing/adapters/jest";
+import { TestProvider, TestResource } from "./helper";
+setupJest();
 
 test("context can be passed through CDKTF_CONTEXT", () => {
   process.env[CONTEXT_ENV] = JSON.stringify({
@@ -127,3 +131,76 @@ test("app synth executes Aspects", () => {
 });
 
 class MyResource extends TerraformResource {}
+
+describe("Cross Stack references", () => {
+  class OriginStack extends TerraformStack {
+    public resource: TestResource;
+    constructor(scope: App, id: string) {
+      super(scope, id);
+
+      new TestProvider(this, "TestProvider", {});
+
+      this.resource = new TestResource(this, "resource", {
+        name: "resource",
+      });
+    }
+  }
+  let app: App;
+  let originStack: OriginStack;
+  let testStack: TerraformStack;
+
+  beforeAll(() => {
+    const outdir = fs.mkdtempSync(path.join(os.tmpdir(), "cdktf.outdir."));
+    console.log(outdir);
+    app = Testing.stubVersion(new App({ stackTraces: false, outdir }));
+    originStack = new OriginStack(app, "OriginStack");
+    testStack = new TerraformStack(app, "TestStack");
+    new TestProvider(testStack, "TestProvider", {});
+  });
+
+  function getStackSynths(app: App): {
+    originStackSynth: string;
+    targetStackSynth: string;
+  } {
+    const originStackSynth = fs.readFileSync(
+      path.resolve(app.outdir, "stacks", "OriginStack", "cdk.tf.json"),
+      "utf8"
+    );
+    const targetStackSynth = fs.readFileSync(
+      path.resolve(app.outdir, "stacks", "TestStack", "cdk.tf.json"),
+      "utf8"
+    );
+    return { originStackSynth, targetStackSynth };
+  }
+
+  it.only("creates remote state and output", () => {
+    new TestResource(testStack, "Resource", {
+      name: originStack.resource.stringValue,
+    });
+
+    app.synth();
+    const { originStackSynth, targetStackSynth } = getStackSynths(app);
+
+    expect(Object.keys(JSON.parse(originStackSynth).output).length).toBe(1);
+    expect(targetStackSynth).toHaveDataSource(DataTerraformRemoteStateLocal);
+  });
+
+  it.todo("passes backend configuration to remote state definition");
+  it.todo("uses the same remote state as the origin stack");
+  it.todo("uses assets for local state");
+
+  it.todo("creates a dependency graph between stacks in manifest");
+  it.todo("throws an error when a stack is referenced from a different app");
+  it.todo("throws an error when there is a circular stack dependency");
+
+  it.todo("references primitive values");
+  it.todo("references complex values");
+  it.todo("references nested values");
+
+  it.todo("references terraform function output");
+  it.todo("can use reference in terraform function");
+
+  it.todo("references can be passed through stacks");
+
+  it.todo("one reference can be used in multiple stacks");
+});
